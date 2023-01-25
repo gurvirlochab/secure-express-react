d233fd493dd19f6479ff8e715c24d79633b14880
Gurvir Lochab <gur.lochab@gmail.com>
2023-01-25 22:54:51 +0300

    Added helmet, helmet-csp

--- server/index.js
+++ server/index.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const helmet = require('helmet')
+const helmetCSP = require('helmet-csp')
+const rateLimit = require('express-rate-limit')
 const prisma = require('./prisma/prisma')
 
 // const prisma = new PrismaClient()
 
 require('dotenv').config()
 
+//Use helmet to set various HTTP headers
+app.use(helmet())
+
 // Use cors to configure CORS settings
 app.use(cors())
 
+// Use helmet-csp to set the Content-Security-Policy header
+app.use(
+    helmetCSP({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", 'trusted-scripts.com'],
+            styleSrc: ["'self'", 'trusted-styles.com'],
+            imgSrc: ["'self'", 'trusted-images.com'],
+        },
+    })
+)
+
 app.use(express.json())
+
+// Use rate limiting to prevent DoS attacks
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+})
+app.use(limiter)
+
 
 app.get('/', (req, res) => {
     return res.json({
